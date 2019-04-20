20bf521a3c40e27980687feea8f397ec81422918
Bruno Giovanini <bsgiovanini@gmail.com>
2019-04-20 11:00:57 -0300

    first implementation

--- app/src/index.js
+++ app/src/index.js
@@ -15,7 +15,7 @@ const App = {
       const deployedNetwork = starNotaryArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
         starNotaryArtifact.abi,
-        deployedNetwork.address,
+        deployedNetwork.address
       );
 
       // get accounts
@@ -35,15 +35,18 @@ const App = {
     const { createStar } = this.meta.methods;
     const name = document.getElementById("starName").value;
     const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
+    await createStar(name, id).send({ from: this.account });
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
   // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
+  lookUp: async function() {
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const id = document.getElementById("lookid").value;
+    const name = await lookUptokenIdToStarInfo(id).call({ from: this.account });
+    console.log(name);
+    App.setStatus("The Star Name is " + name + ".");
   }
-
 };
 
 window.App = App;
@@ -54,9 +57,13 @@ window.addEventListener("load", async function() {
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    console.warn(
+      "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live"
+    );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://127.0.0.1:9545")
+    );
   }
 
   App.start();
--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,6 +14,9 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
+    string public constant name = "BSG Udacity Token";
+    string public constant symbol = "BSGUT"; 
+    //uint8 public constant decimals = 18;
     
 
     // mapping the Star with the Owner Address
@@ -57,20 +60,32 @@ contract StarNotary is ERC721 {
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
+        require(bytes(tokenIdToStarInfo[_tokenId].name).length > 0);
+        return tokenIdToStarInfo[_tokenId].name;       
     }
 
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
-        //2. You don't have to check for the price of the token (star)
+        address ownerToken1 = ownerOf(_tokenId1);
+        address ownerToken2 = ownerOf(_tokenId2);
+        require(ownerToken1 == msg.sender || ownerToken2 == msg.sender);
+        //2. You don't have to check for the price of the token (star)       
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
-        //4. Use _transferFrom function to exchange the tokens.
+        //4. Use _transferFrom function to exchange the tokens.   
+        _transferFrom(ownerToken1, ownerToken2, _tokenId1);
+        _transferFrom(ownerToken2, ownerToken1, _tokenId2);
+        
     }
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
-        //1. Check if the sender is the ownerOf(_tokenId)
+         //1. Check if the sender is the ownerOf(_tokenId)
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        address ownerToken = ownerOf(_tokenId);     
+        require(ownerToken == msg.sender);   
+        _transferFrom(ownerToken, _to1, _tokenId);
+       
     }
 
 }
